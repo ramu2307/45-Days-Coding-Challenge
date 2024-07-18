b00899a9c5ee7d814a56fd505b4a3c52e901f225
ramu2307 <rajaram.krr01@gmail.com>
2024-07-18 15:29:47 +0530

    Modified 02-sort-colors

--- 01-Arrays/02-Sort-Colors.java
+++ 01-Arrays/02-Sort-Colors.java
@@ -1,2 +1,33 @@
-hloooo
+class Solution {
+    public void sortColors(int[] nums) {
+        int zeroCnt = 0;
+        int oneCnt = 0;
+        int twoCnt = 0;
+        for(int i = 0;i < nums.length;i++) {
+            if(nums[i] == 0) {
+                zeroCnt++;
+            }
+            else if(nums[i] == 1) {
+                oneCnt++;
+            }
+            else{
+                twoCnt++;
+            }
+        }
+
+        int iter = 0;
+        for(int i = 0;i < zeroCnt;i++) {
+            nums[iter] = 0;
+            iter++;
+        }
+        for(int i = 0;i < oneCnt;i++) {
+            nums[iter] = 1;
+            iter++;
+        }
+        for(int i = 0;i < twoCnt;i++) {
+            nums[iter] = 2;
+            iter++;
+        }
+    }
+}
 
